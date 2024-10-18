37e0f3ffe4b8d9563d5aeb6464b01e0979e0577a
LCGhit <luispcunha@proton.me>
2024-10-18 12:10:20 +0100

    server ignores message with given number

--- UDPPingerServer.py
+++ UDPPingerServer.py
@@ -14,12 +14,14 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', int(sys.argv[1])))
 
 while True:
-	# Generate random number in the range of 0 to 10
-	rand = random.randint(0, 10)
-	# Receive the client packet along with the address it is coming from
-	message, address = serverSocket.recvfrom(1024)
-	message = message.decode("utf-8") # convert bytes to string
-	# Capitalize the message from the client
-	message = message.upper()
-	#the server responds
-	serverSocket.sendto(message.encode("utf-8"), address)
+    # Generate random number in the range of 0 to 10
+    rand = random.randint(0, 10)
+    # Receive the client packet along with the address it is coming from
+
+    message, address = serverSocket.recvfrom(1024)
+    if (int(message.split()[1]) != 2):
+        message = message.decode("utf-8") # convert bytes to string
+        # Capitalize the message from the client
+        message = message.upper()
+        # the server responds
+        serverSocket.sendto(message.encode("utf-8"), address)
